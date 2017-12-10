0de31e7f589303a263bef1ed7c4058c556a51466
altuga@gmail.com <upux1234>
2017-12-11 00:11:22 +0300

    small format changes

--- prototype/src/main/java/com/iluwatar/prototype/App.java
+++ prototype/src/main/java/com/iluwatar/prototype/App.java
@@ -48,20 +48,24 @@ public class App {
    */
   public static void main(String[] args) {
     HeroFactory factory;
+
     Mage mage;
     Warlord warlord;
     Beast beast;
 
     factory = new HeroFactoryImpl(new ElfMage(), new ElfWarlord(), new ElfBeast());
+
     mage = factory.createMage();
     warlord = factory.createWarlord();
     beast = factory.createBeast();
+
     LOGGER.info(mage.toString());
     LOGGER.info(warlord.toString());
     LOGGER.info(beast.toString());
 
     factory = new HeroFactoryImpl(new OrcMage(), new OrcWarlord(), new OrcBeast());
     mage = factory.createMage();
+
     warlord = factory.createWarlord();
     beast = factory.createBeast();
     LOGGER.info(mage.toString());
