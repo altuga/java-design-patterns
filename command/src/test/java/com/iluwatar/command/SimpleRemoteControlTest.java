153a9ff581954fe69ab4c237dbd78625a2f55a25
altuga@gmail.com <upux1234>
2017-12-19 22:18:26 +0300

    big changes in the code

--- command/src/test/java/com/iluwatar/command/SimpleRemoteControlTest.java
+++ command/src/test/java/com/iluwatar/command/SimpleRemoteControlTest.java
@@ -5,6 +5,8 @@ import com.iluwatar.command.helper.InMemoryAppender;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertTrue;
 
 public class SimpleRemoteControlTest {
@@ -40,5 +42,39 @@ public class SimpleRemoteControlTest {
         // then
         assertTrue(appender.logContains("Light is on"));
         assertTrue(appender.logContains("Garage Door is Open"));
+    }
+
+    @Test
+    public void testCommandPolimorfik() {
+        // given
+        SimpleRemoteControl remote = new SimpleRemoteControl();
+        ArrayList<Command> commandArrayList = new ArrayList<Command>();
+
+        GarageDoor garageDoor = new GarageDoor();
+        Light light = new Light();
+
+        // when
+        GarageDoorOpenCommand garageOpen =
+                new GarageDoorOpenCommand(garageDoor);
+        commandArrayList.add(garageOpen);
+
+        LightOnCommand lightOn = new LightOnCommand(light);
+        commandArrayList.add(lightOn);
+
+        LightOffCommand lightOffCommand = new LightOffCommand(light);
+        commandArrayList.add(lightOffCommand);
+
+
+        for (Command command: commandArrayList) {
+             command.execute();  // power of polimorfizm
+        }
+
+        // then
+        assertTrue(appender.logContains("Light is on"));
+        assertTrue(appender.logContains("Garage Door is Open"));
+        assertTrue(appender.logContains("Light is off"));
+
+
+
     }
 }
